ae689b8ea0e332a5770ab107e13f59e88335ff50
askowronski <askowronski@backstopsolutions.com>
2018-05-29 17:08:47 -0500

    Starting to add query builder methods

--- src/main/java/CustomEntityProject/PersistenceManager/MongoDbPersistenceManager.java
+++ src/main/java/CustomEntityProject/PersistenceManager/MongoDbPersistenceManager.java
@@ -2,7 +2,9 @@ package CustomEntityProject.PersistenceManager;
 
 import CustomEntityProject.MorphiaSessionFactory;
 import java.util.List;
+import java.util.Map;
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.query.Query;
 
 public class MongoDbPersistenceManager {
 
@@ -40,12 +42,37 @@ public class MongoDbPersistenceManager {
         }
     }
 
+    public Object getEntityByFilter(String filter,String filterValue, Class classType) throws Exception {
+        try {
+            return MorphiaSessionFactory.getDataStore().createQuery(classType).filter(filter, filterValue).get();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
     public List<Object> getEntities(Class classType) throws Exception {
         try {
             return MorphiaSessionFactory.getDataStore().createQuery(classType).asList();
         } catch (Exception e) {
             throw e;
         }
+    }
+
+    public Query beginQuery(Class classType) {
+        return MorphiaSessionFactory.getDataStore().find(classType);
+    }
+
+
+    public Query addRestriction(Query query, String fieldRestricition, String value) {
+        return query.filter(fieldRestricition,value);
+    }
+
+    public Query addRestrictionToSubField(Query query, String subFieldName, String fieldRestriction, String value) {
+        return query.filter(subFieldName+fieldRestriction, value);
+    }
+
+    public Object addHasElement(Query query, String fieldName, Object obj) {
+        return query.field(fieldName).hasThisOne(obj);
     }
 
 }
