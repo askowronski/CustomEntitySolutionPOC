bc51421e086e13ae7cf689dc171b015f8c5cf7a2
askowronski <askowronski@backstopsolutions.com>
2018-05-29 17:06:59 -0500

    Added method to allow for one equals restriction

--- src/main/java/CustomEntityProject/API/CustomEntityApi.java
+++ src/main/java/CustomEntityProject/API/CustomEntityApi.java
@@ -5,12 +5,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import CustomEntityProject.CustomEntity;
-import CustomEntityProject.EntityDefinition;
 import CustomEntityProject.PersistenceManager.CustomEntityManager;
-import CustomEntityProject.PersistenceManager.EntityDefinitionManager;
+import CustomEntityProject.PersistenceManager.Restriction;
+import CustomEntityProject.Value;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,12 +59,47 @@ public class CustomEntityApi {
 
     @CrossOrigin
     @RequestMapping(method = GET, path = "/GetCustomEntities")
-    public ResponseEntity<String> getCustomEntities() {
+    public ResponseEntity<String> getCustomEntities(@RequestParam(required = false) String restrictionExpression, @RequestParam(required = false) String operatorString,
+            @RequestParam(required = false) String value ) {
 
         try {
             CustomEntityManager manager = new CustomEntityManager();
-            List<CustomEntity> entities = manager.getAllCustomEntities();
-            return ResponseEntity.ok(CustomEntityManager.writeCustomEntitiesToJson(entities));
+            Value valueToRestrictOn = new Value();
+            valueToRestrictOn.setAvailabilityKey(restrictionExpression);
+            valueToRestrictOn.setValue(value);
+            return ResponseEntity.ok(CustomEntityManager.writeCustomEntitiesToJson(
+                    StringUtils.isEmpty(restrictionExpression) ? manager.getAllCustomEntities() : manager.getCustomEntitiesWithValue(valueToRestrictOn)));
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @CrossOrigin
+    @RequestMapping(method = POST, path = "/Create1000Entities")
+    public ResponseEntity<String> create1000entities() {
+
+        try {
+            CustomEntityManager manager = new CustomEntityManager();
+            List<String> availabilityKeys = new ArrayList<>();
+            String shf = "show_cfield";
+            availabilityKeys.add(shf+22043);
+            availabilityKeys.add(shf+22045);
+            availabilityKeys.add(shf+22047);
+            availabilityKeys.add(shf+22049);
+            availabilityKeys.add(shf+22051);
+            availabilityKeys.add(shf+22053);
+            availabilityKeys.add(shf+22055);
+            availabilityKeys.add(shf+22057);
+            availabilityKeys.add(shf+22059);
+            availabilityKeys.add(shf+22061);
+            availabilityKeys.add(shf+22063);
+            availabilityKeys.add(shf+22065);
+            availabilityKeys.add(shf+22067);
+            availabilityKeys.add(shf+22069);
+            availabilityKeys.add(shf+"PartyBean.name");
+            manager.create1000Entities(availabilityKeys);
+
+            return ResponseEntity.ok("noice");
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
